b00f0b6109f459397bb4be6ab395d2c1c5019f7f
jjangsky <dbcks4646@gmail.com>
2025-01-07 23:29:59 +0900

    feat: 사용자 이름 설정 및 메세지 내역에서 사용자 확인 처리

--- chat-service/src/main/java/com/addict/jjangsky/chatservice/handlers/WebSocketChatHandler.java
+++ chat-service/src/main/java/com/addict/jjangsky/chatservice/handlers/WebSocketChatHandler.java
@@ -8,6 +8,10 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 @Component
 public class WebSocketChatHandler extends TextWebSocketHandler {
@@ -16,21 +20,37 @@ public class WebSocketChatHandler extends TextWebSocketHandler {
      * 스프링에서 제공하는 TextWebSocketHandler 사용
      */
 
+    // 연결되어 있는 클라이언트 확인 Map
+    final Map<String, WebSocketSession> webScoketSessionMap = new ConcurrentHashMap<>();
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         // 웹소켓 클라이언트가 서버로 연결한 이후 동작
         log.info("{}", session.getId());
+
+        // 세션 연결
+        this.webScoketSessionMap.put(session.getId(), session);
     }
 
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
         // 메세지가 왔을 때 처리 로직
-        super.handleMessage(session, message);
+        log.info("{}, {}", session.getId(), message.getPayload());
+
+        this.webScoketSessionMap.values().forEach(webSocketSession -> {
+            try {
+                webSocketSession.sendMessage(message);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         // 서버에 접속해 있던 웹소켓 클라이언트가 연결을 끊었을 때
         log.info("{}",session.getId());
+
+        this.webScoketSessionMap.remove(session.getId());
     }
 }
